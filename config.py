91effaa63247d03a005dad4439723d3901d1416a
Kayfarik47 <aasrorov097@gmail.com>
2026-08-13 22:07:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,9 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GROUP_ID = int(os.getenv("GROUP_ID", "-1004317972340"))
-ADMINS = [6262166970, 515523963]
+ADMINS = [6262166970, 515523963, 6262616970]
 
 # Hisobot topshirish uchun so'nggi muddat (shundan keyin "Kechikdi" deb belgilanadi)
 DEADLINE_HOUR = int(os.getenv("DEADLINE_HOUR", "14"))
 DEADLINE_MINUTE = int(os.getenv("DEADLINE_MINUTE", "0"))
+
